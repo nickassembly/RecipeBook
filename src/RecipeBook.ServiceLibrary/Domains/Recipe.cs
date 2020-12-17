489f154cf56b40ab5f181933d9e91dff02cb8f86
nickguerra <nickguerra@gmail.com>
2020-12-17 06:48:50 -0600

    reorganized folder structure

---
+++ src/RecipeBook.ServiceLibrary/Domains/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using RecipeBook.ServiceLibrary.Entities;
+using RecipeBook.ServiceLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.ServiceLibrary.Domains
+{
+    public class Recipe
+    {
+      public void SaveRecipe(RecipeEntity recipeEntity)
+      {
+         // Validate things
+         var repository = new RecipeRepository();
+         repository.SaveRecipeToDatabase(recipeEntity);
+      }
+   }
+}
