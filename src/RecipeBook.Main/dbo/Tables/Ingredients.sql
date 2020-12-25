2437fac978a8fb45f9386e29ccac95b11853f6f4
nickguerra <nickguerra@gmail.com>
2020-12-25 08:41:29 -0600

    added database

---
+++ src/RecipeBook.Main/dbo/Tables/Ingredients.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Ingredients]
+(
+	[RecipeId] UNIQUEIDENTIFIER NOT NULL ,
+	[OrdinalPosition] INT NOT NULL,
+	[Unit] NVARCHAR(25) NOT NULL,
+	[Quantity] float NOT NULL,
+	[Ingredient] NVARCHAR(50) NOT NULL,
+	CONSTRAINT [FK_Ingredients_Recipe_RecipeId] FOREIGN KEY ([RecipeId]) REFERENCES [Recipes]([Id]) ON DELETE CASCADE
+
+)
